10dbabf30547eb65f144b3aeb3fb29e865466115
Clément François <clement.francois.developpeur@outlook.com>
2022-04-24 17:02:39 +0200

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PanelComponent } from './panel/panel.component';
+import { LikeComponent } from './like/like.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PanelComponent,
+    LikeComponent
   ],
   imports: [
     BrowserModule
